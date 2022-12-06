81e99b26e4df181e14eaab3f286c9101ce8608d1
hemg2 <hemg2@naver.com>
2022-12-06 17:42:11 +0900

    상태변화에 따른 앱델리게이트메서드

--- Interview/CS/상태 변화에 따라 다른 동작을 처리하기 위한 앱델리게이트 메서드들을 설명하기.swift
+++ Interview/CS/상태 변화에 따라 다른 동작을 처리하기 위한 앱델리게이트 메서드들을 설명하기.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+/*
+ 
+ 
+ application
+
+ 델리게이트에게 프로세스 준비가 거의 완료가 되었고
+ 앱이 거의 시작되었다고 알려주는 메소드
+
+ applicationWillTerminate
+
+ 앱이 끝난다는걸 델리게이트에게 알려주는 메소드
+
+ applicationDidBecomeActive
+
+ 앱이 Active 상태에 들어간다는걸 델리게이트에게 알려주는 메소드
+
+ applicationWillResignActive
+
+ 앱이 inactive 상태에 들어간다는걸 델리게이트에게 알려주는 메소드
+
+ applicationDidFinishLaunching
+
+ 앱 론칭이 끝났다는걸 델리게이트에게 알려주는 메소드, 사용하지 말래요... 맨위에 있는거를 사용하라네요??
+
+ applicationDidEnterBackground
+
+ 앱이 지금 백그라운드 상태에 들어간다는걸 델리게이트에게 알려주는 메소드
+
+ applicationWillEnterForeground
+
+ 앱이 지금 포그라운드 상태에 들어간다는걸 델리게이트에게 알려주는 메소드
+ */
